d4fcd619376c3550bf5a936c853fece4b7ba9cef
Jonathan Owens <jonathano@metia.com>
2018-11-05 14:18:24 -0800

    Just updated the story

--- src/components/ChapterOne.js
+++ src/components/ChapterOne.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ChapterOne = (props) => {
   return (
     <p className="story-paragraph">
-      Last night she had gathered trash and whatever else was nearby and less shit stained than that old Serta, covered it sufficiently, and hopped right in. Sleep itself was almost as scarce as food. Quality sleep, anyway. You could learn to conk out under almost any circumstance, but she had learned sleeping deeply enough to feel rested was dangerous.
+      A low frequency drone radiated from somewhere in the room, rattling his bones in a rhythmic pulse. He scanned, looking for a door, a window, a corner. Nothing. The soles of his shoes rested on a floor that was firm but not really there.
     </p>
   )
 }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,8 +3,7 @@ import React from 'react';
 const Intro = (props) => {
   return (
     <p className="story-paragraph">
-      Marley managed to lift her corpse of a body from the mattress she had spent the night on. She was starving this morning, just like every morning, and would have killed everyone within a fifty mile radius for some bread or cheese. Even for a woman who was naturally dainty, she looked unsalvageable.
-
+      Tom sat on a metal stool, in a room with walls that looked like black and purple obsidian; Walls that appeared to be, somehow, breathing. Not in the same way air enters and exits a lung. The motion was cold, like a machine trying to pass as living.
     </p>
   )
 }
